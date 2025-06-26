51a458fa31add6794f539ac8e648f0c6286aed56
MDOT <mohammadmunti.5@gmail.com>
2025-06-26 17:52:27 +0530

    added more in select list

--- src/Components/SelectList.jsx
+++ src/Components/SelectList.jsx
@@ -1,50 +1,54 @@
 import React from 'react'
 
 
-const SelectList = ({itemId,handleInputChange,selectedCategory}) => {
+const SelectList = ({ itemId, handleInputChange, selectedCategory }) => {
   return (
     <select
-    className="border-1"
-    id="medicine-form"
-    name="category"
-    value={selectedCategory}  // Bind the state value
-    onChange={(e) => handleInputChange(itemId, "category", e.target.value)} // Update state when selection changes
-    required
-  >
-    <option value="">Select Category</option>  {/* Default placeholder */}
-    <option value="Tablet">Tablet</option>
-    <option value="Capsule">Capsule</option>
-    <option value="Syrup">Syrup</option>
-    <option value="Suspension">Suspension</option>
-    <option value="Drops">Drops</option>
-    <option value="Injection">Injection</option>
-    <option value="Ointment">Ointment</option>
-    <option value="Cream">Cream</option>
-    <option value="Gel">Gel</option>
-    <option value="Powder">Powder</option>
-    <option value="Suppository">Suppository</option>
-    <option value="Inhaler">Inhaler</option>
-    <option value="Patch">Patch</option>
-    <option value="Lozenge">Lozenge</option>
-    <option value="Spray">Spray</option>
-    <option value="Test Kit">TEST KIT</option>
-    <option value="Diaper">DIAPER</option>
-    <option value="Sanitary Pads">SANITARY PADS</option>
-    <option value="Sanitizer">SANITIZER</option>
-    <option value="Granules">Granules</option>
-    <option value="Baby Food">BABY FOOD</option>
-    <option value="Lotion">LOTION</option>
-    <option value="Cosmetics">COSMETICS</option>
-    <option value="Sachet">SACHET</option>
-    <option value="Mouth Wash">MOUTHWASH</option>
-    <option value="Soap">SOAP</option>
-    <option value="Cream">CREAM</option>
-    <option value="Oil">Oil</option>
-    <option value="Wipes">Wipes</option>
-    <option value="Rotocap">ROTOCAP</option>
-    
-    
-  </select>
+      className="border-1"
+      id="medicine-form"
+      name="category"
+      value={selectedCategory}  // Bind the state value
+      onChange={(e) => handleInputChange(itemId, "category", e.target.value)} // Update state when selection changes
+      required
+    >
+      <option value="">Select Category</option>  {/* Default placeholder */}
+      <option value="Tablet">Tablet</option>
+      <option value="Capsule">Capsule</option>
+      <option value="Syrup">Syrup</option>
+      <option value="Suspension">Suspension</option>
+      <option value="Drops">Drops</option>
+      <option value="Injection">Injection</option>
+      <option value="Ointment">Ointment</option>
+      <option value="Cream">Cream</option>
+      <option value="Gel">Gel</option>
+      <option value="Powder">Powder</option>
+      <option value="Suppository">Suppository</option>
+      <option value="Inhaler">Inhaler</option>
+      <option value="Patch">Patch</option>
+      <option value="Lozenge">Lozenge</option>
+      <option value="Spray">Spray</option>
+      <option value="Test Kit">TEST KIT</option>
+      <option value="Diaper">DIAPER</option>
+      <option value="Sanitary Pads">SANITARY PADS</option>
+      <option value="Sanitizer">SANITIZER</option>
+      <option value="Granules">Granules</option>
+      <option value="Baby Food">BABY FOOD</option>
+      <option value="Lotion">LOTION</option>
+      <option value="Cosmetics">COSMETICS</option>
+      <option value="Sachet">SACHET</option>
+      <option value="Mouth Wash">MOUTHWASH</option>
+      <option value="Soap">SOAP</option>
+      <option value="Cream">CREAM</option>
+      <option value="Oil">Oil</option>
+      <option value="Wipes">Wipes</option>
+      <option value="Rotocap">ROTOCAP</option>
+      <option value="Rotocap">NANO SHOTS</option>
+      <option value="Rotocap">SACHETS</option>
+      <option value="Rotocap">GARGLES</option>
+      <option value="Rotocap">HANDWASH</option>
+      <option value="Rotocap">SUSPENSION</option>
+      <option value="Rotocap">HAIR PRODUCT</option>
+    </select>
   )
 }
 
