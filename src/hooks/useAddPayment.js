db1dabbc3e696415f9b8a554d51c0c680beaea3f
MDOT <mohammadmunti.5@gmail.com>
2025-10-28 23:02:18 +0530

    Refactor: created custom hook for add new payment

---
+++ src/hooks/useAddPayment.js
@@ -0,0 +1,26 @@
+import { addPaymentSql } from "@/services/medicineService.js";
+export const addNewPayment = async (e,newPaymentDetails,setNewPaymentDetails) => {
+    e.preventDefault();
+    let newPayment = { ...newPaymentDetails, id: Date.now(), type: 'Payment' }
+    setNewPaymentDetails(newPayment);
+    try {
+        const response = await addPaymentSql(newPayment);
+        console.log(response);
+        setNewPaymentDetails({
+            date: '',
+            invoice: '',
+            supplierName: '',
+            drugLicenseNumber: '',
+            total: ''
+        })
+
+        alert('Payment Added Successfully!');
+
+
+    } catch (error) {
+        console.error('Error Adding Payment to the database', error);
+        alert('Failed To Add Payment');
+    }
+
+
+}
